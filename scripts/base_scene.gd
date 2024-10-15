f5f9c5099fbf2f72428a4697175efa8af8c2cae1
zarifmahir <zarifmahir21@gmail.com>
2024-10-16 01:53:18 +0600

    update scene change

---
+++ scripts/base_scene.gd
@@ -0,0 +1,23 @@
+class_name BaseScene extends Node
+
+@onready var player: Detect = $Detective
+@onready var entrancemarkers: Node2D = $EntranceMarkers
+func _ready() -> void:
+	if Scene_Manager.player:
+		if player:
+			player.queue_free()
+		
+		player = Scene_Manager.player
+		add_child(player)
+		
+	position_player()
+
+func position_player():
+	var last_scene = Scene_Manager.last_scene_name
+	if last_scene.is_empty():
+		last_scene = "any"
+	for entrance in entrancemarkers.get_children():
+		if entrance is Marker2D and entrance.name == "any" or entrance.name == last_scene:
+			player.global_position = entrance.global_position
+			
+	
--- scripts/insidebuilding_1.gd
+++ scripts/insidebuilding_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-
+@onready var player = $Detective
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Scene_Manager.layer:
-		add_child(Scene_Manager.layer)
+	if Scene_Manager.player:
+		add_child(Scene_Manager.player)
