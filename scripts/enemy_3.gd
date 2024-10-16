3cfff5ae77ca178bb6e88d45bda17e704669b69a
zarifmahir <zarifmahir21@gmail.com>
2024-10-17 01:36:27 +0600

    enemy and world changes

---
+++ scripts/enemy_3.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+var pl: Detect
+var speed = 50
+var player_chase = false
+var player = null
+@onready var animations = $AnimatedSprite2D
+
+	
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/speed 
+		updateVelocity()
+		move_and_slide()
+		updateAnimation()
+	else: 
+		animations.play("idle")
+		
+func updateVelocity():
+	var moveDirection = position
+	velocity = moveDirection.normalized()*speed
+
+func updateAnimation():
+	if velocity.length()==0:
+		if(animations.is_playing()):
+			animations.stop()
+	else:
+		var direc = player.direction
+		animations.play("walk"+direc)
+
+
+		
+func _on_detectionarea_body_entered(body: Node2D) -> void:
+	if body is Detect:
+		player = body
+		player_chase = true
+
+
+func _on_detectionarea_body_exited(body: Node2D) -> void:
+	if body is Detect:
+		player = body
+		player_chase = true
